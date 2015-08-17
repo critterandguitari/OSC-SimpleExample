b247a081e62a8e9ff157efb16f4bad41158ba46e
Owen <owen@Owens-MacBook-Pro.local>
2015-08-17 08:37:10 -0400

    added udp send receive

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ extern "C" {
 
 #include "OSC/OSCMessage.h"
 #include "OSC/SimpleWriter.h"
-#include "udp_send.h"
+
+#include "UdpSocket.h"
 
 //SLIPEncodedSerial SLIPSerial;
 
@@ -29,15 +30,22 @@ void renumber(OSCMessage &msg){
 int main(int argc, char* argv[]) {
       
     uint32_t seconds = 0;
-    uint8_t osc_message[256];
+    char osc_packet_in[256];
+    //uint8_t osc_packet_in[256];
     uint8_t i = 0;
+    uint32_t len = 0;
 
-    udp_send_init();
+
+    UdpSocket udpinsock(4001);
+    UdpSocket udpoutsock(4004);
+    
+    //udp_send_init();
+    //pd_receive_init();
 
     printf("cool\n");
     //the message wants an OSC address as first argument
     OSCMessage msg("/thing/test");
-    msg.add(909);
+    msg.add(888);
         
     dump.start();  
     msg.send(dump);
@@ -49,7 +57,19 @@ int main(int argc, char* argv[]) {
         printf("%x ", dump.buffer[i]);
     }
 
-    udp_send_buf(dump.buffer, dump.length);
+    //udp_send_buf(dump.buffer, dump.length);
     //pd_udp_send_buf
-    printf("\ndone\n");
+    udpoutsock.setDestination(4000, "localhost");
+    udpoutsock.writeBuffer(dump.buffer, dump.length);
+
+    printf("\ndone, now gonna receive\n");
+
+    for (;;){
+      //  len = pd_receive_poll(osc_packet_in);
+        //len = udpread(osc_packet_in);
+        len = udpinsock.readBuffer(osc_packet_in, 256, 0);
+        if (len > 0)
+            printf("received %d bytes\n", len);
+    }
+
 }
